67ac5b6398e769de7cd15a7ec6377f7dc2f42a83
Dodik Dwi Sancoko <dodikds@gmail.com>
2026-02-08 21:21:43 +0700

    perbaiki repositories/transaction_repository.go ketika insert transaction details ke db

--- repositories/transaction_repository.go
+++ repositories/transaction_repository.go
@@ -71,9 +71,14 @@ func (repo *TransactionRepository) CreateTransaction(items []models.CheckoutItem
 	}
 
 	// insert transaction details
-	for i, detail := range details {
+	// for i, detail := range details {
+	// 	details[i].TransactionID = transactionID
+	// _, err := tx.Exec("INSERT INTO transaction_details (transaction_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4)", transactionID, detail.ProductID, detail.Quantity, detail.Subtotal)
+
+	for i := range details {
 		details[i].TransactionID = transactionID
-		_, err := tx.Exec("INSERT INTO transaction_details (transaction_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4)", transactionID, detail.ProductID, detail.Quantity, detail.Subtotal)
+		_, err = tx.Exec("INSERT INTO transaction_details (transaction_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4)",
+			transactionID, details[i].ProductID, details[i].Quantity, details[i].Subtotal)
 		if err != nil {
 			return nil, err
 		}
